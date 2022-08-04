edb4843bdf2eeab5e013a1f6e92e5f3af22e47b5
Lucaponz91 <lucapo1991@msn.com>
2022-08-04 16:57:47 +0200

    done, add loop, setInterval

--- js/slider.js
+++ js/slider.js
@@ -33,3 +33,107 @@ const slides = [
 			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
 	},
 ]
+const slideElements = []
+
+console.log(slides[0].url)
+
+// Riprendiamo l’esercizio carosello e rifacciamolo, questa volta usando gli oggetti, prendendo come riferimento il codice che trovate allegato,
+// Bisogna generare il murkup delle slide ciclando l’array di oggetti che trovare nel file js.
+// Il murkup da generare invece lo potrete trovare nel file html.
+// A parte la generezione del murkup, il resto della logica è la stessa del vecchio esercizio,
+
+// RECUPERO LO SLIDESWRAPPERELEMENT
+const slidesWrapperEl = document.querySelector('.slides-wrapper')
+// const slide = document.querySelector('.slide')
+let currentIndex = 0
+// LO SVUOTO
+slidesWrapperEl.innerHTML = ""
+for (i = 0; i < slides.length; i++){
+
+	const src = slides[i].url;
+	// CREO L' LI
+	const li = document.createElement('li')
+	// AGGIUNGO LA CLASSE SLIDE ALL' LI
+	li.classList = 'slide'
+	// IMMAGINE DI PARTENZA
+	if (i === currentIndex) {
+		li.classList.add('active')
+	}
+
+	// CREO IMG
+	const img = document.createElement ('img')
+	// ASSEGNO ALLA PROPRIETà SRC DI IMG IL VALORE SRC DELL'ARRAY
+	img.src = src
+	// INSERISCO IMG DENTRO LI 
+	li.append(img)
+	// AGGIUNGO LI ALLO SLIDESWRAPPER
+	slidesWrapperEl.append(li)
+	// CREO IL DIV, AGGIUNGO CLASSE E LO APPENDO ALL' LI
+	const div = document.createElement('div')
+	div.classlist = 'slide__content'
+	li.append(div)
+	// AGGIUNGO H3 E P DENTRO IL DIV
+	div.innerHTML += `
+	<h3 class="slide__title">${slides[i].title} </h3>
+	<p class="slide__description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem suscipit aspernatur dicta laudantium delectus inventore itaque tempora quo earum, facilis nam, officia necessitatibus esse voluptates harum. Repudiandae nesciunt quod sunt!</p>
+	`;
+	// PUSHO GLI LI CREATI NELL'ARRAY DA ME CREATO
+	slideElements.push(li)
+}
+console.log(slideElements)
+
+
+// // PESCO LA FRECCIA AVANTI DALL'HTML
+const nextElement = document.querySelector('.arrow-next');
+// AGGIUNGO L'EVENT LISTENER
+nextElement.addEventListener('click', function () {
+    // TOLGO CLASSE ACTIVE DALLA SLIDE ATTIVA
+    slideElements[currentIndex].classList.remove('active');
+
+    // AGGIUNGO LA CLASSE ACTIVE ALLA SLIDE SEGUENTE
+    // BONUS 1 LOOP
+    if (currentIndex === slideElements.length - 1) {
+        currentIndex = 0;
+		let slideAttiva = slideElements[currentIndex];
+        slideAttiva.classList.add('active');
+    } else {
+		currentIndex++
+		let slideAttiva = slideElements[currentIndex];
+        slideAttiva.classList.add('active');
+    }
+})
+
+// PESCO LA FRECCIA INDIETRO DALL'HTML
+const prevArrowElement = document.querySelector('.arrow-prev')
+// AGGIUNGO L'EVENT LISTENER
+prevArrowElement.addEventListener('click', function () {
+// 	// TOLGO CLASSE ACTIVE DALLA SLIDE ATTIVA
+	slideElements[currentIndex].classList.remove('active');
+	// LOOP
+	if (currentIndex === 0) {
+        currentIndex = slideElements.length - 1;
+		let slideAttiva = slideElements[currentIndex];
+        slideAttiva.classList.add('active');
+    } else {
+		currentIndex--
+		let slideAttiva = slideElements[currentIndex];
+        slideAttiva.classList.add('active');
+    }
+})
+
+let autoslide = setInterval(autoNext, 3000);
+
+// ###### FUNZIONE AUTOSLIDER
+function autoNext() {
+	slideElements[currentIndex].classList.remove('active');
+	if (currentIndex === slideElements.length - 1) {
+        currentIndex = 0;
+		let slideAttiva = slideElements[currentIndex];
+        slideAttiva.classList.add('active');
+    } else {
+		currentIndex++
+		let slideAttiva = slideElements[currentIndex];
+        slideAttiva.classList.add('active');
+    }
+
+}
